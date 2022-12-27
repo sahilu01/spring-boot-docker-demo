7cb3d423fc2c931dc82ff54d8190b24ebec21481
Sahil Sharma <sahil.sharma@gdn-commerce.com>
2022-12-27 20:27:53 +0530

    build fix

--- src/main/java/com/personal/project/springbootdockerdemo/Config.java
+++ src/main/java/com/personal/project/springbootdockerdemo/Config.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -32,15 +31,6 @@ public class Config {
   @Bean
   public KafkaTemplate<String, String> kafkaTemplate() {
     return new KafkaTemplate<>(producerFactory());
-  }
-
-  @Bean
-  public NewTopic topic() {
-    return TopicBuilder
-        .name("hello-world")
-        .partitions(1)
-        .replicas(1)
-        .build();
   }
 
 }
